08e97d0fb32573036d9f8d61896c4fe4c55262a5
Samuel095383 <samuel095383@gmail.com>
2025-12-27 09:09:56 +0100

    updated the install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ NQPTP_VERSION="1.2.4"
 SHAIRPORT_SYNC_VERSION="4.3.2"
 TMP_DIR=""
 
+sudo apt install git
+
 cleanup() {
     if [ -d "${TMP_DIR}" ]; then
         rm -rf "${TMP_DIR}"
@@ -22,7 +24,7 @@ verify_os() {
 
     . /etc/os-release
 
-    if [[ ("$ID" != "debian" && "$ID" != "raspbian") || "$VERSION_ID" != "12" ]]; then
+    if [[ ("$ID" != "debian" && "$ID" != "raspbian") || "$VERSION_ID" != "13" ]]; then
         echo $MSG
         exit 1
     fi
@@ -112,7 +114,8 @@ install_shairport() {
     if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then return; fi
 
     sudo apt update
-    sudo apt install -y --no-install-recommends wget unzip autoconf automake build-essential libtool git autoconf automake libpopt-dev libconfig-dev libasound2-dev avahi-daemon libavahi-client-dev libssl-dev libsoxr-dev libplist-dev libsodium-dev libavutil-dev libavcodec-dev libavformat-dev uuid-dev libgcrypt20-dev xxd
+    sudo apt install --no-install-recommends build-essential git autoconf automake libtool libpopt-dev libconfig-dev libasound2-dev avahi-daemon libavahi-client-dev libssl-dev libsoxr-dev libplist-dev libsodium-dev libavutil-dev libavcodec-dev libavformat-dev uuid-dev libgcrypt-dev xxd libalac
+
 
     if [[ -z "$TMP_DIR" ]]; then
         TMP_DIR=$(mktemp -d)
@@ -133,26 +136,24 @@ install_shairport() {
     rm -rf alac-master
 
     # Install NQPTP
-    wget -O nqptp-${NQPTP_VERSION}.zip https://github.com/mikebrady/nqptp/archive/refs/tags/${NQPTP_VERSION}.zip
-    unzip nqptp-${NQPTP_VERSION}.zip
-    cd nqptp-${NQPTP_VERSION}
+    git clone https://github.com/mikebrady/nqptp.git
+    cd nqptp
     autoreconf -fi
     ./configure --with-systemd-startup
     make -j $(nproc)
     sudo make install
     cd ..
-    rm -rf nqptp-${NQPTP_VERSION}
+    rm -rf nqptp
 
     # Install Shairport Sync
-    wget -O shairport-sync-${SHAIRPORT_SYNC_VERSION}.zip https://github.com/mikebrady/shairport-sync/archive/refs/tags/${SHAIRPORT_SYNC_VERSION}.zip
-    unzip shairport-sync-${SHAIRPORT_SYNC_VERSION}.zip
-    cd shairport-sync-${SHAIRPORT_SYNC_VERSION}
+    git clone https://github.com/mikebrady/shairport-sync.git
+    cd shairport-sync
     autoreconf -fi
     ./configure --sysconfdir=/etc --with-alsa --with-soxr --with-avahi --with-ssl=openssl --with-systemd --with-airplay-2 --with-apple-alac
     make -j $(nproc)
     sudo make install
     cd ..
-    rm -rf shairport-sync-${SHAIRPORT_SYNC_VERSION}
+    rm -rf shairport-sync
 
     # Configure Shairport Sync
     sudo tee /etc/shairport-sync.conf >/dev/null <<EOF
